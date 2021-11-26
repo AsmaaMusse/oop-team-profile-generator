4b8950551f7a1b5471539c0abc6ec51db5883244
Asmaa <Asmaa99musse@gmail.com>
2021-11-26 01:32:28 +0000

    Added questions array to index.js

--- src/Index.js
+++ src/Index.js
@@ -1,8 +1,34 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
 const path = require("path");
 
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of the employee?",
+  },
+
+  {
+    type: "input",
+    name: "id",
+    message: "What is the Id of the employee?",
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "What is this email of this employee?",
+  },
+
+  {
+    type: "list",
+    name: "role",
+    message: "Which employee role do you want to add?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+];
