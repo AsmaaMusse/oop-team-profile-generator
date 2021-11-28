0f8f1f0b875ca634830e9f6fe869e118e8f7513d
Asmaa <Asmaa99musse@gmail.com>
2021-11-28 16:54:05 +0000

    created generate HTML function on index.js

--- src/Index.js
+++ src/Index.js
@@ -152,27 +152,152 @@ const start = async () => {
   writeToFile("generatedHTML.md", html);
 };
 
-const generateTitle = (answers) => {
-  return ``;
+const generateHtmlIntro = () => {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  
+      <link
+        rel="stylesheet"
+        href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+        crossorigin="anonymous"
+      />
+  
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,700;0,900;1,100;1,200;1,600&display=swap"
+        rel="stylesheet"
+      />
+  
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+        crossorigin="anonymous"
+      />
+  
+      <link rel="stylesheet" href="./styles.css" />
+      <title>My Team</title>
+    </head>`;
+};
+
+const generateTeamName = (answers) => {
+  return `<body>
+  <header class="header">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 jumbotron mb-3 bg-dark text-white">
+          <h1 class="text-center">
+            <i class="fas fa-users"></i> ${answers.teamName}
+          </h1>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div class="container">
+    <div class="row">
+      <div
+        class="team-area col-12 d-flex flex-wrap justify-content-between p-3"
+      >`;
 };
 
 const generateManagerCard = (answers) => {
-  return ``;
+  return `<div class="card employee-card mr-1 mt-3">
+  <div class="card-header">
+    <h2 class="card-title">${answers.name}</h2>
+    <h3 class="card-title">Manager</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-group">
+      <li class="list-group-item">
+        <span class="material-icons">fingerprint</span> ID: ${answers.id}
+      </li>
+      <li class="list-group-item text-dark">
+        <span class="material-icons">email</span> Email:
+        <a href="mailto:{{ email }}">${answers.email}</a>
+      </li>
+      <li class="list-group-item">
+        <span class="material-icons">business</span> Office Number:
+        ${answers.officeNumber}
+      </li>
+    </ul>
+  </div>
+</div>
+`;
 };
 
 const generateEngineerCard = (answers) => {
-  return ``;
+  return `<div class="card employee-card mr-1 mt-3">
+  <div class="card-header">
+    <h2 class="card-title">${answers.name}</h2>
+    <h3 class="card-title bg-info">Engineer</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-group">
+      <li class="list-group-item">
+        <span class="material-icons"></span> ID: ${answers.id}
+      </li>
+      <li class="list-group-item text-dark">
+        <span class="material-icons">email</span> Email:
+        <a href="mailto:{{ email }}">${answers.email}</a>
+      </li>
+      <li class="list-group-item text-dark">
+        <span class="material-icons">code</span> GitHub:
+        <a href="https://github.com/{{ github }}" target="_blank"
+          >${answers.github}</a
+        >
+      </li>
+    </ul>
+  </div>
+</div>
+`;
 };
 
 const generateInternCard = (answers) => {
-  return ``;
+  return `<div class="card employee-card mr-1 mt-3">
+  <div class="card-header">
+    <h2 class="card-title">${answers.name}</h2>
+    <h3 class="card-title">Intern</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-group">
+      <li class="list-group-item">
+        <span class="material-icons">fingerprint</span> ID: ${answers.id}
+      </li>
+      <li class="list-group-item text-dark">
+        <span class="material-icons">email</span> Email:
+        <a href="mailto:{{ email }}">${answers.email}</a>
+      </li>
+      <li class="list-group-item">
+        <span class="material-icons">school</span> School: ${answers.school}
+      </li>
+    </ul>
+  </div>
+</div>
+`;
+};
+
+const generateHtmlEnd = () => {
+  return ` </div>
+  </div>
+</div>
+</body>
+</html>
+`;
 };
 
 const generateHtml = (answers) => {
-  return `${generateTitle(answers)}
+  return `${generateHtmlIntro(answers)}
+  ${generateTeamName(answers)}
   ${generateManagerCard(answers)}
   ${generateEngineerCard(answers)}
-  ${generateInternCard(answers)}`;
+  ${generateInternCard(answers)}
+  ${generateHtmlEnd(answers)}`;
 };
 
 const writeToFile = (filePath, htmlFile) => {
