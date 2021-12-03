b52c63c0e017daae5ee295ba9728e976bb11774c
Asmaa <Asmaa99musse@gmail.com>
2021-12-03 04:51:15 +0000

    created the first test for employee file

---
+++ src/lib/__test__/Employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require("../Employee");
+
+describe("Employee", () => {
+  test("should be an Employee", () => {
+    const instance = new Employee("Employee");
+    const expected = "Employee";
+    const actual = instance.getRole();
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("should return expected name", () => {
+    const instance = new Employee({ name: "James Smith" });
+    const expected = "James Smith";
+    const actual = instance.getName();
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("should return expected Id", () => {
+    const instance = new Employee({ id: 575 });
+    const expected = "575";
+    const actual = instance.getId();
+
+    expected(actual).toEqual(expected);
+  });
+
+  test("should return the expected email", () => {
+    const instance = new Employee({ email: "james.smith07@gmail.com" });
+    const expected = "james.smith07@gmail.com";
+    const actual = instance.getEmail();
+
+    expected(actual).toEqual(expected);
+  });
+});
